cd65a7494229afa2b319aab20689d31624f1d72b
SushiWaUmai <eugene.bb1@gmail.com>
2022-06-06 21:50:30 +0200

    feat: :sparkles: add simple grid

--- src/main/kotlin/org/example/Processing.kt
+++ src/main/kotlin/org/example/Processing.kt
@@ -2,19 +2,27 @@ package org.example
 
 import processing.core.PApplet
 import processing.core.PConstants
+import processing.core.PVector
 
 class Processing : PApplet() {
+    private var gridSize: PVector = PVector(32f, 32f)
 
     override fun settings() {
-        size(640, 360, PConstants.JAVA2D)
+        size(800, 800, PConstants.JAVA2D)
     }
 
     override fun setup() {
-
     }
 
     override fun draw() {
+        background(0)
+        stroke(255f, 0f, 0f)
 
+        for (y in 1 until gridSize.y.toInt()) {
+            line(0f, y * height / gridSize.y, width.toFloat(), y * height / gridSize.y)
+        }
+        for (x in 1 until gridSize.x.toInt()) {
+            line(x * width / gridSize.x, 0f, x * width / gridSize.x, height.toFloat())
+        }
     }
-
 }
