ee9681edb602f0b419a365ea3e7c536eab62e8e1
SushiWaUmai <eugene.bb1@gmail.com>
2022-06-07 15:52:05 +0200

    feat: :sparkles: iniitialize an array of random values

--- src/main/kotlin/org/example/Processing.kt
+++ src/main/kotlin/org/example/Processing.kt
@@ -3,26 +3,29 @@ package org.example
 import processing.core.PApplet
 import processing.core.PConstants
 import processing.core.PVector
+import kotlin.random.Random
 
 class Processing : PApplet() {
-    private var gridSize: PVector = PVector(32f, 32f)
+    private var gridSize: PVector = PVector(8f, 8f)
+    private var grid: FloatArray = FloatArray(gridSize.x.toInt() * gridSize.y.toInt()) { Random.nextFloat() }
 
     override fun settings() {
         size(800, 800, PConstants.JAVA2D)
     }
 
     override fun setup() {
+        println(grid.contentToString())
     }
 
     override fun draw() {
         background(0)
         stroke(255f, 0f, 0f)
 
-        for (y in 1 until gridSize.y.toInt()) {
-            line(0f, y * height / gridSize.y, width.toFloat(), y * height / gridSize.y)
+        for (y in 1 until gridSize.y.toInt() - 1) {
+            line(0f, y * height / (gridSize.y - 1), width.toFloat(), y * height / (gridSize.y - 1))
         }
-        for (x in 1 until gridSize.x.toInt()) {
-            line(x * width / gridSize.x, 0f, x * width / gridSize.x, height.toFloat())
+        for (x in 1 until gridSize.x.toInt() - 1) {
+            line(x * width / (gridSize.x - 1), 0f, x * width / (gridSize.x - 1), height.toFloat())
         }
     }
 }
